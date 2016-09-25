9b6b3f52187e7e593c79531d7da999bec123ee6d
Dillettant <xinhant@gmail.comgit config user.email xinhant@gmail.com>
2016-09-25 16:21:03 -0700

    Revert "Revert "BLE Heartrate""
    This reverts commit 8ec250f2dc2b24919a1b92e6c6584fcaeaec67e0.

---
+++ src/org/bluetooth/bledemo/Point.java
@@ -0,0 +1,22 @@
+package org.bluetooth.bledemo;
+
+public class Point {
+	
+	private int x;
+	private float y;
+	
+	public Point( int x, float audioBuf)
+	{
+		this.x = x;
+		this.y = audioBuf;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+	
+}
